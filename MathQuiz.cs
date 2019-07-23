aae0cc95a7c59e1026ac90419406da1ffb230acf
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-23 18:13:53 -0400

    Add files via upload

---
+++ MathQuiz.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch7Homework
+{
+    class MathQuiz
+    {
+        static Random rand = new Random();
+        int score;
+      public MathQuiz()
+        {
+            score = 0;
+        }
+
+        public void AskQuestion(Difficulty diff)
+        {
+            if (diff == Difficulty.Easy)
+            {
+               // Random rand = new Random();
+                int num1 = rand.Next(1, 100);
+                int num2 = rand.Next(1, 100);
+                Console.WriteLine("What is {0} + {1}", num1, num2);
+                int ans = Convert.ToInt32(Console.ReadLine());
+
+                int correct = num1 + num2;
+
+                if (ans == correct)
+                {
+                    score++;
+                    ShowCorrectMessage();
+                   
+                }
+                else
+                {
+                    ShowIncorrectMessage();
+                }
+            }
+
+            if (diff == Difficulty.Medium)
+            {
+                //Random rand = new Random();
+                int num1 = rand.Next(50, 5000);
+                int num2 = rand.Next(50, 5000);
+                Console.WriteLine("What is {0} + {1}", num1, num2);
+                int ans = Convert.ToInt32(Console.ReadLine());
+
+                int correct = num1 + num2;
+
+                if (ans == correct)
+                {
+                    score++;
+                    ShowCorrectMessage();
+                }
+                else
+                {
+                    ShowIncorrectMessage();
+                }
+            }
+            if (diff == Difficulty.Hard)
+            {
+              //  Random rand = new Random();
+                int num1 = rand.Next(500, 5000);
+                int num2 = rand.Next(500, 5000);
+                Console.WriteLine("What is {0} + {1}", num1, num2);
+                int ans = Convert.ToInt32(Console.ReadLine());
+
+                int correct = num1 + num2;
+
+                if (ans == correct)
+                {
+                    score++;
+                    ShowCorrectMessage();
+                }
+                else
+                {
+                    ShowIncorrectMessage();
+                }
+            }
+        }
+        
+        public void ShowCorrectMessage()
+        {
+          //  Random rand = new Random();
+            int num1 = rand.Next(1, 5);
+
+            switch (num1)
+            {
+                case 1:
+                    Console.WriteLine("Good");
+                    break;
+                case 2:
+                    Console.WriteLine("Great");
+                    break;
+                case 3:
+                    Console.WriteLine("Excellent");
+                    break;
+                case 4:
+                    Console.WriteLine("Phenomenal");
+                    break;
+            }
+        }
+        public void ShowIncorrectMessage()
+        {
+          //  Random rand = new Random();
+            int num1 = rand.Next(1, 5);
+
+            switch(num1)
+            {
+                case 1:
+                    Console.WriteLine("wrong");
+                    break;
+                case 2:
+                    Console.WriteLine("incorrect");
+                    break;
+                case 3:
+                    Console.WriteLine("sorry");
+                    break;
+                case 4:
+                    Console.WriteLine("try another time");
+                    break;
+            }
+        }
+
+        public void ShowScore()
+        {
+            Console.WriteLine("Your score {0}", score);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch7Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter choices (E)asy, (M)edium, or (H)ard");
+
+            MathQuiz n = new MathQuiz();
+            string choice = Console.ReadLine();
+
+            if (choice == "E")
+            {
+                int counter = 1;
+                while (counter <= 10)
+                {
+                    n.AskQuestion(Difficulty.Easy);
+                    counter++;
+                }
+                n.ShowScore();
+            }
+            if (choice == "M")
+            {
+                int counter = 1;
+                while (counter <= 10)
+                {
+                    n.AskQuestion(Difficulty.Medium);
+                    counter++;
+                }
+                n.ShowScore();
+            }
+            if (choice == "H")
+            {
+                int counter = 1;
+                while (counter <= 10)
+                {
+                    n.AskQuestion(Difficulty.Hard);
+                    counter++;
+                }
+                n.ShowScore();
+            }
+            Console.WriteLine("PAK");
+            Console.ReadKey();
+        }
+    }
+}
